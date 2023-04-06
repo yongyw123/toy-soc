352478e245cd68b32f74d576ff32bbd567007d79
yyick <yongyickw@gmail.com>
2023-04-07 02:39:27 +1000

    added test bench for general purpose input core

--- dev_soc/dev_soc.srcs/sim_1/new/core_gpi_tb.sv
+++ dev_soc/dev_soc.srcs/sim_1/new/core_gpi_tb.sv
@@ -1,15 +1,15 @@
-`timescale 1ns / 10ps
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
 // 
-// Create Date: 07.04.2023 00:55:39
+// Create Date: 07.04.2023 02:25:20
 // Design Name: 
-// Module Name: core_gpo_tb
+// Module Name: core_gpi_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description:  
+// Description: 
 // 
 // Dependencies: 
 // 
@@ -20,34 +20,26 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module core_gpo_tb();
+module core_gpi_tb();
     // general;
     localparam T = 10;  // clock period: 10ns;
     logic clk;
     logic reset;
-    localparam OUTPUT_WIDTH = 8;        // output data width size;
+    localparam INPUT_WIDTH = 8;        // input data width size;
     localparam DATA_WIDTH = 32;         // microblaze uses 32-bit data;
     localparam REG_ADDRESS_WIDTH = 5;   // each io core has 2**5 registers;
+    
     // main input of the uut;
     logic cs;                       // input; chip select;
     logic write;                    // input;
     logic read;                     // input;
-    logic [REG_ADDRESS_WIDTH -1:0] addr;  // register address;
+    logic [REG_ADDRESS_WIDTH -1:0] addr;  // input; register address;
     logic [DATA_WIDTH-1:0] wr_data; // input;
     logic [DATA_WIDTH-1:0] rd_data; // input;
-    logic [OUTPUT_WIDTH-1:0] dout;
+    logic [INPUT_WIDTH-1:0] din;
     
-    
-    /*
-    * expectation (what could go wrong);
-    * 1.  only inputs: write AND cs will trigger a write operation;
-    * 2. input: read;  no effect;
-    * 3. output: dout: truncated to W-size;
-    *
-    */
-
     // uut instantiate;
-    core_gpo #(.W(OUTPUT_WIDTH)) uut(.*);
+    core_gpi #(.W(INPUT_WIDTH)) uut(.*);
             
     // simulate clk
     always
@@ -68,35 +60,44 @@ module core_gpo_tb();
         @(negedge clk); // avoid data setup and hold time for subsequent simulation;
     end
     
-    
     /* 
     * what to test?
-    * control signal: cs and write vs dout;
-    * four combinations;
-    * {cs, write};
+    * as of now, the GPI core is constructed as a simple 
+    * register to sample the input without relying any control signals 
+    * of the core interface;
+    *
+    * so, check:
+    * 1. whether the input is sampled correctly
+    * 2. the sampled data is truncated correctly before passed as output;
     */
     bit[3:0] i;
+    
     initial
     begin
-        // go beyond 4 patterns with randomized random data;
-        // by above, only expect the dout to be updated with cs and write set;
-        // otherwise, dout should maintain as it is;
         for(i = 0; i <= 4'b0111 ; i++) begin 
             @(negedge clk);
+            // control signals should not matter but included just in case;
             cs = i[0];
             write = i[1];
-            read = i[0];            // expect to be ignored;
-            wr_data = 32'h2 * 10'($random);  // some random pattern;
-            addr = 5'h12345;         // expect to be ignored;
+            read = i[0];                     // expect to be ignored;
+            wr_data = 32'h2 * 10'($random);  // expect to be ignored;
+            addr = 5'h12345;                // expect to be ignored;
+            
+            // main stimulus;
+            din = INPUT_WIDTH'($random);
+            
+            /* main response to check: rd_data;*/
             
             // it takes one clock cycle to update the output;
             @(negedge clk);
             
-            $display("time: %0t, index: %0B, cs: %0b, write: %0b, wr_data: %0B, dout: %0b",
-                     $time, i, cs, write, wr_data, dout);
-        end
-        
+            $display("time: %0t, index: %0B, cs: %0b, write: %0b, wr_data: %0B, din: %0B, rd_data: %0B",
+                     $time, i, cs, write, wr_data, din, rd_data);
+    
+        end    
     $display("done");
     $stop;
-    end 
+    
+    
+    end
 endmodule
