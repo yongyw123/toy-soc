f6f236146fd2aaaa01d71709716080299936acb4
yyick <yongyickw@gmail.com>
2023-04-07 19:04:32 +1000

    updated mapping header

--- dev_soc/dev_soc.srcs/sources_1/new/IO_map.svh
+++ dev_soc/dev_soc.srcs/sources_1/new/IO_map.svh
@@ -31,22 +31,40 @@
 // system clock; fixed at 100MHz;
 `define SYS_CLK_FREQ 100   
 
+
+/*---------------------------------------------------- 
+* bus info/ (address)
+----------------------------------------------------
+// microblaze mcs bus is 32-bit;
+// after bridging, the user-bus is 2?-bit; (it is still undecided);
+// that said user-bus size is at most 32-bit;
+// and there should be no harm allocating more for the user-bus;
+// one concern is the fpga size; not sure if this will be a limiting factor?
+*/
+`define BUS_MICROBLAZE_SIZE_G 32
+`define BUS_USER_SIZE_G       21
+
 // IO based address provided by microblaze MSC, as above;
-`define MCS_IO_BUS_BASE_ADDR 0xC0000000
+`define BUS_MICROBLAZE_IO_BASE_ADDR 0xC0000000
 
-// size;
+/*---------------------------------------------------- 
+* mmio address space 
+----------------------------------------------------*/
+`define MIMO_ADDR_SIZE_G        6                   // mmio to accommodate 64 cores;
+//`define MIMO_CORE_TOTAL_G       2**MIMO_ADDR_SIZE_G // 64 cores;
+`define MIMO_CORE_TOTAL_G       64 // 64 cores;
 
-`define MIMO_ADDR_SIZE_G    6                   // mmio to accommodate 64 cores;
-`define MIMO_CORE_TOTAL     2**MIMO_ADDR_SIZE_G // 64 cores;
-
+/* ----------------------------------------------------
+* register info of each core; 
+----------------------------------------------------*/
 `define REG_DATA_WIDTH_G    32  // MCS uses word (32-bit);
 `define REG_ADDR_SIZE_G     5   // each core has 2^{5} = 32 internal registers;
 
-/*
+/*----------------------------------------------------
 * IO modules/cores shall be sloted in the IO memory map;
 * module index; each module is allocated with 32 registers;
 * max number of cores is still undecided yet;
-*/  
+----------------------------------------------------*/  
 `define S0_SYS_TIMER    0   // timer;
 `define S1_DEBUG_UART   1   // uart for serial debugging;
 `define S2_GPO_LED      2   // general purpose output to accommodate LED;
