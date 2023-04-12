f3e69568369d41837c34c4345743cb39defd4aa8
yyick <yongyickw@gmail.com>
2023-04-12 21:59:09 +1000

    elaborated some comments for documentation purposes

--- dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/FIFO_tb.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/FIFO_tb.sv
@@ -35,7 +35,13 @@ program FIFO_tb
         @(posedge clk)
         ctrl_wr <= 1'b0;
         ctrl_rd <= 1'b0;
-        /* test 01: write-only until fifo is full */
+        /* test 01: write-only until fifo is full 
+        expectation:
+        1. full_flag will be triggered eventually;
+        2. read_data will always point to the base (the first written data)
+            because there is no read request;
+        */
+        
         // go beyond the address width to capture fifo status flags;
         // such as full/empty;
         for(int i = 0; i < ADDR_WIDTH + 5; i++) begin        
@@ -44,7 +50,11 @@ program FIFO_tb
             wr_data = (DATA_WIDTH)'($random);
         end
         
-        /* test 02: read-only until fifo is empty */
+        /* test 02: read-only until fifo is empty 
+        expectation;
+        1. the read data will be the write data in test 01 in a first-in-first out manner;
+        2. empty_flag will be triggered eventually;
+        */
         @(posedge clk)
         ctrl_wr <= 1'b0;
         ctrl_rd <= 1'b0;
@@ -53,7 +63,13 @@ program FIFO_tb
             ctrl_rd <= 1'b1;
         end 
         
-        /* test 03: read and write when fifo is already empty */
+        /* test 03: read and write when fifo is already empty 
+        expectation:
+        1. empty flag will always be triggered;
+        2. read_data will be the same as the write_data
+            because the write and read pointers are pointing
+            at the same place;
+        */
         @(posedge clk)
         ctrl_wr <= 1'b0;
         ctrl_rd <= 1'b0;
@@ -61,9 +77,14 @@ program FIFO_tb
             @(posedge clk)
             ctrl_rd <= 1'b1;
             ctrl_wr <= 1'b1;
+            wr_data = (DATA_WIDTH)'($random);
         end 
         
-        /* test 03: read and write when fifo is not empty and not full */
+        /* test 03: read and write when fifo is not empty and not full 
+        expectation:
+        1. full and empty flags will never occur;
+        2. the read data will "lag" the write data by the amount of the write apriori;  
+        */
         @(posedge clk)
         ctrl_wr <= 1'b0;
         ctrl_rd <= 1'b0;
