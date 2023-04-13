e7bccd99ba43c854696ffed0031c07164e1b9cb3
yyick <yongyickw@gmail.com>
2023-04-14 01:20:44 +1000

    added class header for uart core

---
+++ dev-soc/sw/user_src/core_uart.h
@@ -0,0 +1,66 @@
+#ifndef _CORE_UART_H
+#define _CORE_UART_H
+
+/* ---------------------------------------------
+Purpose: SW drivers for uart core;
+---------------------------------------------*/
+
+#include "io_map.h"
+#include "io_reg_util.h"
+#include "inttypes.h"
+
+// c and cpp linkage;
+// reference: https://igl.ethz.ch/teaching/tau/resources/cprog.htm
+#ifdef __cpluscplus
+extern "C" {
+#endif
+
+class core_uart{
+    // register map;
+    enum{
+        REG_STATUS_OFFSET = S1_UART_REG_STATUS_OFFSET, 
+        REG_BAUD_OFFSET = S1_UART_REG_BAUD_OFFSET,   
+        REG_TX_WRITE_RQ_OFFSET = S1_UART_REG_TX_WRITE_REQUEST_OFFSET,
+        REG_RX_READ_RQ_OFFSET = S1_UART_REG_RX_READ_REQUEST_OFFSET,
+        REG_RX_READ_DATA_OFFSET = S1_UART_REG_RX_READ_DATA_OFFSET
+    };
+
+    // masking for the status flag of the status register;
+    enum{
+        
+        STATUS_RX_EMPTY = BIT_MASK(S1_UART_REG_STATUS_BIT_POS_RX_EMPTY),
+        STATUS_TX_FULL = BIT_MASK(S1_UART_REG_STATUS_BIT_POS_TX_FULL)
+    };
+
+
+    public:
+        // general;
+        core_uart(uint32_t core_base_addr);    // constructor
+        ~core_uart(); // destructor;
+        
+        // setting;
+        void set_baud_rate(uint32_t baud_rate); // set baud rate;
+        
+        // status
+        uint32_t check_rx_fifo_empty(void);
+        uint32_t check_tx_fifo_full(void);
+        
+        // rw
+        void uart_tx_byte(uint8_t data);
+        void uart_rx_byte(void);
+
+        // print;
+        void print(const char *str);
+    
+    private:
+        uint32_t base_addr;
+        uint32_t baud_rate;
+};
+
+
+#ifdef __cpluscplus
+} // extern "C";
+#endif
+
+
+#endif //_CORE_UART_H
