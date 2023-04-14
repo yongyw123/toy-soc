0c6921b069326c37443ac89a76077c83f222dd76
yyick <yongyickw@gmail.com>
2023-04-14 19:22:33 +1000

    added uart test case to the main program

--- dev-soc/sw/user_src/main.cpp
+++ dev-soc/sw/user_src/main.cpp
@@ -9,22 +9,11 @@ core_gpi obj_sw(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S3_GPI_SW));
 core_gpio obj_jumper(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S4_GPIO_PORT));
 
 int main(){
-    uint32_t port01 = 0;
-	uint32_t port03 = 0;
-
-    // set to output direction;
-    obj_jumper.set_direction(0, 1);     // port 0; write
-    obj_jumper.set_direction(1, 0);     // port 1; read;
-    obj_jumper.set_direction(2, 1);     // port 2; write
-    obj_jumper.set_direction(3, 0);     // port 3; read;
-
+    
     while(1){
-        port01 = obj_jumper.read(1);
-        port03 = obj_jumper.read(3);
-
-        obj_led.write(1, port01);
-        obj_led.write(3, port03);
-
+        test_uart();
+        debug("pause for 2 seconds");
+        delay_busy_ms(2000); // one seconds;
     }
 }
 
