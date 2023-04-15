aba8370dbf2455314f8a852325fa460d2ec2f3ab
yyick <yongyickw@gmail.com>
2023-04-16 01:09:25 +1000

    created a source file for spi

---
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/spi_sys.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.04.2023 01:08:28
+// Design Name: 
+// Module Name: spi_sys
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: Serial Peripheral Interface Circuit 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+
+*/
+
+module spi_sys(
+
+    );
+endmodule
