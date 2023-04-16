778038f3d80f0bcfe274ed8699d2153109520f97
yyick <yongyickw@gmail.com>
2023-04-16 22:35:31 +1000

    created empty simulation files for spi core wrapper

---
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_spi_top_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.04.2023 22:35:03
+// Design Name: 
+// Module Name: core_spi_top_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module core_spi_top_tb(
+
+    );
+endmodule
