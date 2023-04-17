3edf0d41314eb8e0443942a7fa1d7c9b4f855575
yyick <yongyickw@gmail.com>
2023-04-18 02:20:47 +1000

    updated main app to test spi core

--- dev-soc/sw/user_src/main.cpp
+++ dev-soc/sw/user_src/main.cpp
@@ -9,7 +9,8 @@ core_spi obj_spi(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S5_SPI));
 
 int main(){
     
-    test_spi_mosi(&obj_spi);
+    //test_spi_mosi(&obj_spi);
+    test_spi_device_lcd_ili9341(&obj_spi);
 
     while(1){
         //test_uart();    
