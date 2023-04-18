3ef2e1463a4fa3b8ac984fbd8242c8e393e11c90
yyick <yongyickw@gmail.com>
2023-04-18 16:30:16 +1000

    drafted i2c controller

--- dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/i2c_controller.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/i2c_controller.sv
@@ -60,5 +60,120 @@ module i2c_master_controller
         inout tri sda   // this line is shared between master and slaves;
     );
     
+    // misc constants;
+    localparam DATA_BIT_W = $clog2(I2C_DATA_BIT);
+    
+    // command constants;
+    localparam CMD_START    = 3'b000;   // generate start condition;
+    localparam CMD_WR       = 3'b001;   // master write to slave;
+    localparam CMD_RD       = 3'b010;   // master reads from slave;
+    localparam CMD_STOP     = 3'b011;   // generate stop condition;
+    localparam CMD_REPEAT   = 3'b100;   // generate repeated_start condition;
+        
+    /* state;
+    the state definitions are
+    defined in the linked doc above;
+    */
+    typedef enum{
+        // i2c is free;
+        ST_IDLE,    
+        
+        // roughly when SCL and SDA must hold low after start, and prior to stop;
+        ST_HOLD,
+        
+        // start phases 
+        ST_START_01,
+        ST_START_02,
+        
+        // actual data phases;
+        ST_DATA_01,
+        ST_DATA_02,
+        ST_DATA_03,
+        ST_DATA_04,
+        
+        // stop phases;
+        ST_STOP_01,
+        ST_STOP_02,
+        
+        // end condition;
+        ST_DATA_END,
+        
+        // repeat start condition
+        ST_RESTART
+    }state_type;
+    
+    /* signals; */
+    state_type state_reg, state_next;
+    logic [I2C_CLK_WIDTH-1:0] clk_cnt_reg, clk_cnt_next;    // scl clock counter;
+
+    // scl clock period divided to the four (quarter) phases of the data; 
+    logic [I2C_CLK_WIDTH-1:0] phase_quarter;     
+    
+    // scl clock period divided to the two (half) phases of the start, end; 
+    logic [I2C_CLK_WIDTH-1:0] phase_half;
+    
+    // registers;
+    logic [I2C_TOTAL_CMD_NUM-1:0] cmd_reg, cmd_next;    // to store the user command;
+    logic [I2C_DATA_BIT-1:0] tx_reg, tx_next;   // master write to slave;
+    logic [I2C_DATA_BIT-1:0] rx_reg, rx_next;  // slave to master;
+    logic [DATA_BIT_W-1:0]  data_cnt_reg, data_cnt_next;   // track the number of data bits
+    logic sda_reg, sda_next;    // to control sda line;
+    logic scl_reg, scl_next;    // to control scl line;
+    
+    // indication and status;
+    logic phase_data;   // to track which data phase we are in to determine wr, rx actions;
+    logic sda_hiz;      // when to set sda line high impedance;
+    logic nack;         // to stop reading;
+    
+    // ff;
+    always_ff @(posedge clk, posedge reset)
+    begin
+        if(reset)
+        begin
+            state_reg <= ST_IDLE;   
+            clk_cnt_reg <= 0;
+            cmd_reg <= CMD_START;
+            tx_reg <= 0;
+            rx_reg <= 0;
+            data_cnt_reg <= 0;
+            sda_reg <= 0;
+            scl_reg <= 0;
+        end
+        
+        else
+        begin
+            state_reg <= state_next;   
+            clk_cnt_reg <= clk_cnt_next;
+            cmd_reg <= cmd_next;
+            tx_reg <= tx_next;
+            rx_reg <= rx_next;
+            data_cnt_reg <= data_cnt_next;
+            sda_reg <= sda_next;
+            scl_reg <= scl_next;
+        end
+    end 
+    
+    // fsm;
+    always_comb
+    begin
+        // remain as it is until told otherwise;
+        
+        case (state_reg)
+            ST_IDLE: begin
+            end
+            ST_START_01: begin
+            
+            end
+        endcase
+    end
+    
+    // output;
+        
+    
+     
+    
+     
+         
     
 endmodule
+
