ee7433f358a260f457cf0e124be4d6bcb091df60
yyick <yongyickw@gmail.com>
2023-04-18 16:05:53 +1000

    added module argument for i2c controller

--- dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/i2c_controller.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/i2c_controller.sv
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 /* 
-purpose: i2c controller;
+purpose: i2c master controller;
 assumption;
 1. only one master (so no arbitration);
 2. no clock stretching;
@@ -30,7 +30,35 @@ construction;
 https://docs.google.com/document/d/1ry1kXqD7P2slyMiV86lYMIhMr_kSJ_o6/edit?usp=share_link&ouid=109361905057067991130&rtpof=true&sd=true
 
 */
-module i2c_controller(
-
+module i2c_master_controller
+    #(parameter
+        I2C_CLK_WIDTH = 32,       // for i2c clock counter;
+        I2C_TOTAL_CMD_NUM = 3,   // number of i2c command: start, stop, repeat_start etc..?
+        I2C_DATA_BIT = 8
+     )
+    (
+        /* general; */
+        input logic clk,    // 100 Mhz;
+        input logic reset,  // async;
+        
+        /* i2c specific */
+        // user input;
+        input logic [I2C_CLK_WIDTH-1:0] cnt_mod,    // counter modulus;
+        input logic [I2C_TOTAL_CMD_NUM-1:0] cmd,    // what command: stop, start,?
+        input logic wr_i2c_start,                   // initiate the i2c master;
+        input logic [I2C_DATA_BIT-1:0] din,             // i2c write data;
+        
+        // user output;
+        output logic ready_flag,    // idle;
+        output logic done_flag,     // just finish i2c communications;
+        output logic ack_flag,      // from slave for processing to determine if slave has ack;
+        output logic [I2C_DATA_BIT-1:0] dout,   // slave data;
+        
+        // i2c actual hw pins;
+        // must be tristate by i2c construction/specs;
+        output tri scl, // clock is always initiated by the master;
+        inout tri sda   // this line is shared between master and slaves;
     );
+    
+    
 endmodule
