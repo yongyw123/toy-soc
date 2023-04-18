ad804350b79d2000e6a92b9d548425de78555434
yyick <yongyickw@gmail.com>
2023-04-18 15:50:49 +1000

    created a src file for i2c controller

---
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/i2c_controller.sv
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.04.2023 15:48:35
+// Design Name: 
+// Module Name: i2c_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+/* 
+purpose: i2c controller;
+assumption;
+1. only one master (so no arbitration);
+2. no clock stretching;
+
+construction;
+1. please see this doc linked below
+https://docs.google.com/document/d/1ry1kXqD7P2slyMiV86lYMIhMr_kSJ_o6/edit?usp=share_link&ouid=109361905057067991130&rtpof=true&sd=true
+
+*/
+module i2c_controller(
+
+    );
+endmodule
