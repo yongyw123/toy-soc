69a91081eab54fcadc20e1fb66689dee84d3afe7
yyick <yongyickw@gmail.com>
2023-04-19 17:42:34 +1000

    updated tb for i2c core

--- dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_i2c_master_tb.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_i2c_master_tb.sv
@@ -19,6 +19,10 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+`ifndef CORE_I2C_MASTER_TB_SV
+`define CORE_I2C_MASTER_TB_SV
+
+`include "IO_map.svh"
 
 module core_i2c_master_tb
     (
@@ -31,13 +35,102 @@ module core_i2c_master_tb
         output logic read,
         output logic [`REG_ADDR_SIZE_G-1:0] addr,    
         output logic [`REG_DATA_WIDTH_G-1:0]  wr_data, 
-        output logic [`REG_DATA_WIDTH_G-1:0]  rd_data,
+        input logic [`REG_DATA_WIDTH_G-1:0]  rd_data,
         
         // i2c pins;
         input tri scl,
-        inout tri sda   
+        inout tri sda,
+        
+        // sim;
+        output logic [32:0] test_index  
     );
     
+    localparam sys_freq = 100_000_000;  // 100MHz;
+    //localparam scl_rate_candidate_01 = 2_500_000; // 2.5 MHz;
+    localparam scl_rate_candidate_01 = 25_000_000; // 25 MHz;
+    //localparam scl_program_candidate_mod_01 =  sys_freq/(4*scl_rate_candidate_01) - 1;
+    localparam scl_program_candidate_mod_01 =  sys_freq/(4*scl_rate_candidate_01);
     
+    // command constants;
+    localparam CMD_NOP      = 3'b000;   // no operation;
+    localparam CMD_START    = 3'b001;   // generate start condition;
+    localparam CMD_WR       = 3'b010;   // master write to slave;
+    localparam CMD_RD       = 3'b011;   // master reads from slave;
+    localparam CMD_STOP     = 3'b100;   // generate stop condition;
+    localparam CMD_REPEAT   = 3'b101;   // generate repeated_start condition;
+
+    // register offset;
+    localparam I2C_REG_READ_OFFSET      = 2'b00;  
+    localparam I2C_REG_CLKMOD_OFFSET    = 2'b01; 
+    localparam I2C_REG_WRITE_OFFSET     = 2'b10;
+    
+    // bit pos;
+    localparam I2C_REG_READ_BIT_POS_ACK = 8;
+    localparam I2C_REG_READ_BIT_POS_READY = 9;
+    
+    logic[7:0] master_data;
+    
+    initial 
+    begin
+    $display("test starts");
+    
+    /* test;
+    1. set i2c clock rate;
+    2. start i2c;
+    3. stop i2c;
+    4. read ready flag;
+    
+    */
+    // set clock rate;
+    @(posedge clk);
+    test_index <= 0;
+    write <= 1'b1;
+    read <= 1'b0;
+    cs <= 1'b1;
+    addr <= I2C_REG_CLKMOD_OFFSET;
+    wr_data <= scl_program_candidate_mod_01;
+    
+    // write master data;
+    // note that this action will auto start the i2c controller;
+    master_data = 8'($random);
+    @(posedge clk);
+    test_index <= 1;
+    write <= 1'b1;
+    cs <= 1'b1;
+    addr <= I2C_REG_WRITE_OFFSET;
+    wr_data <= {21'b0, CMD_START, master_data};
+    
+    // wait for the start condittion
+    // read the ready flag then 
+    // issue a write command;
+    
+    //wait(rd_data[I2C_REG_READ_BIT_POS_READY] == 1'b1);
+    //wait(rd_data[9] == 1'b0);
+    wait(rd_data[9] == 1'b1);
+    
+     
+    @(posedge clk);
+    addr <= I2C_REG_WRITE_OFFSET;
+    wr_data <= {21'b0, CMD_WR, master_data};
+    
+    /*
+    // probe the ready flag; 
+    // issue a stop command;
+    @(posedge clk);
+    wait(rd_data[I2C_REG_READ_BIT_POS_READY] == 1'b1);
+    addr <= I2C_REG_WRITE_OFFSET;
+    wr_data <= {21'b0, CMD_STOP, master_data};
+    */
+    wait(rd_data[I2C_REG_READ_BIT_POS_READY] == 1'b1);
+    
+    
+    
+    #(1000);
+    $display("test stops");
+    $stop;
+    end
+                                                                                                                                                                                                                                                                                            
     
 endmodule
+
+`endif //CORE_I2C_MASTER_TB_SV
--- dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_i2c_master_top_tb.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_i2c_master_top_tb.sv
@@ -43,6 +43,70 @@ module core_i2c_master_top_tb();
     logic [`REG_DATA_WIDTH_G-1:0]  rd_data;
     
     // i2c specific;
+    tri scl;
+    tri sda;
+    
+    // debugging;
+    logic [32:0] test_index;
+
+    
+    /* instantiation */
+    
+    core_i2c_master uut
+    (
+        .clk(clk),
+        .reset(reset),
+        .cs(cs),
+        .write(write),
+        .read(read),
+        .addr(addr),
+        .wr_data(wr_data),
+        .rd_data(rd_data),
+        .scl(scl),
+        .sda(sda)
+    );
+    
+    // test stimulus;
+    core_i2c_master_tb tb(.*);
+    
+    /* simulate clk */
+     always
+        begin 
+           clk = 1'b1;  
+           #(T/2); 
+           clk = 1'b0;  
+           #(T/2);
+        end
+    
+     /* reset pulse */
+     initial
+        begin
+            reset = 1'b1;
+            #(T/2);
+            reset = 1'b0;
+            #(T/2);
+        end
+        
+    /* monitoring */
+    initial
+    begin
+        $monitor("time: %0t, test index: %0d, cs: %0b, wr: %0b, rd: %0b, addr: %0B, wrdatad: %0D, wrdatab: %0B, rddata: %0B, scl: %0b, sda: %0b, uut.wr_clkmod: %0b, uut.wr_i2c: %0b",
+            $time,
+            test_index,
+            cs,
+            write,
+            read,
+            addr[2:0],
+            wr_data,
+            wr_data,
+            rd_data,
+            scl,
+            sda,
+            uut.wr_clkmod,
+            uut.wr_i2c,
+           );
+    end
+    
     
     
 endmodule
