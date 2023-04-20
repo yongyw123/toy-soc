ae862aff4b36378c20eae1a517abd227cbe8b66e
yyick <yongyickw@gmail.com>
2023-04-20 21:13:57 +1000

    added camera init functions to the main app

--- dev-soc/sw/user_src/main.cpp
+++ dev-soc/sw/user_src/main.cpp
@@ -8,10 +8,15 @@ core_spi obj_spi(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S5_SPI));
 
 int main(){
     
-    ov7670_test();
+    // initialize camera ov7670;
+    ov7670_hw_reset();
+    delay_busy_ms(1000);    // settingly time;
+    ov7670_write_array(ov7670_basic_init_array);
+    delay_busy_ms(1000);    // settingly time;
+    ov7670_read_array(ov7670_basic_init_array);
 
     while(1){
-        
+        ;        
     }
 }
 
