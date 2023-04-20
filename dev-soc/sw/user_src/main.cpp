cd40024fb07e1c0e12b7dfd8eb7d87b64f5b9221
yyick <yongyickw@gmail.com>
2023-04-20 16:36:17 +1000

    updated main app

--- dev-soc/sw/user_src/main.cpp
+++ dev-soc/sw/user_src/main.cpp
@@ -4,7 +4,6 @@
 /* global instance of each class representation of the IO cores*/
 core_gpo obj_led(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S2_GPO_LED));
 core_gpi obj_sw(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S3_GPI_SW));
-core_gpio obj_gpio(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S4_GPIO_PORT));
 core_spi obj_spi(GET_IO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S5_SPI));
 
 int main(){
