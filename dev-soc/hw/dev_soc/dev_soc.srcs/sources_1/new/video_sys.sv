ebd906f40043f809630a9a8582120dcc98f881c3
yyick <yongyickw@gmail.com>
2023-04-24 23:37:27 +1000

    incorporated lcd and its buffer core into the video system

--- dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/video_sys.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/video_sys.sv
@@ -26,7 +26,18 @@
 
 module video_sys
     #(
-        parameter BITS_PER_PIXEL = 16   // bpp;
+        parameter 
+        // ov7670 camera pixel output is configured to be 16-bit;
+        BITS_PER_PIXEL = 16,   
+        
+        /*
+        // buffer between the src and the lcd display;
+        // it could hold up to 2^6 =64 data of size of 32-bit;
+        // this is equivalent to 16 pixels 
+        */
+        // since the lcd display could only be driven by 8-bit parallel max;
+        LCD_DISPLAY_DATA_WIDTH = 8,
+        FIFO_LCD_ADDR_WITH = 6      // 2^6;
     )
     (
         // general;
@@ -43,11 +54,29 @@ module video_sys
         input logic video_rd,
         input logic [`BUS_USER_SIZE_G-1:0] video_addr,       // addr to decode for IO core address and its register address;
         input logic [`REG_DATA_WIDTH_G-1:0] video_wr_data,   // 32-bit;
-        output logic [`REG_DATA_WIDTH_G-1:0] video_rd_data  // 32-bit;
+        output logic [`REG_DATA_WIDTH_G-1:0] video_rd_data,  // 32-bit;
         
-        /* HW pin mapping (by the constraint file) */
+        /* --------- HW pin mapping (by the constraint file) ------------*/
+        
+        /* LCD display (ILI9341); */
+        output logic lcd_drive_wrx,     //  to drive the lcd for write op;
+        output logic lcd_drive_rdx,     // to drive the lcd for read op;
+        output logic lcd_drive_csx,     // chip select;
+        output logic lcd_drive_dcx,     // data or command; LOW for command;          
+        
+        // this is shared between the host and the lcd;
+        inout tri[LCD_DISPLAY_DATA_WIDTH-1:0] lcd_dinout 
     );
-    // pre cal constants;  
+    
+    
+    /* ----- signal declarations ------*/
+    
+    /* interface between the lcd and the fifo buffer */
+    logic [LCD_DISPLAY_DATA_WIDTH-1:0] lcd_stream_in_pixel_data;
+    logic lcd_stream_valid_flag;
+    logic lcd_stream_ready_flag;
+        
+    /* constants */  
     localparam VIDEO_CORE_NUM_TOTAL = `VIDEO_CORE_TOTAL_G; 
     localparam VIDEO_CORE_BIT_SIZE = $clog2(VIDEO_CORE_NUM_TOTAL);
     localparam VIDEO_REG_BIT_TOTAL = `VIDEO_REG_ADDR_BIT_SIZE_G;    // 19 bit;
@@ -64,8 +93,8 @@ module video_sys
     logic [REG_DATA_WIDTH-1:0] core_data_rd_array[VIDEO_CORE_NUM_TOTAL-1:0]; // read data from each core;
     logic [REG_DATA_WIDTH-1:0] core_data_wr_array[VIDEO_CORE_NUM_TOTAL-1:0]; // write data from each core;
     
-    /* instantiation */
-    // controller;
+    /* -------------------- instantiation ------------------------*/
+    /* video controller; */
     video_ctrl ctrl_unit
     (
         .clk(clk),
@@ -91,6 +120,72 @@ module video_sys
         .core_data_rd_array(core_data_rd_array),    // data to multiplex
         .core_addr_reg_array(core_addr_reg_array)    // register address to decode;      
     );
+    
+     
+    /* fifo interfaceing the lcd display
+    this fifo is buffering between the pixel source
+    and the lcd display;
+    
+    pixel source is either coming from the camera ov7670
+    or from HW pixel generation core;
+    
+    lcd display is the actual ILI9341;
+    this lcd uses MCU 8080-I series;
+    
+    there is a mismatch in the source 
+    rate and the sink rate;
+    
+    so a buffer is necessary;
+    
+    */
+    fifo_core_video_lcd_display 
+    #(
+    .DATA_WIDTH(LCD_DISPLAY_DATA_WIDTH),
+    .ADDR_WIDTH(FIFO_LCD_ADDR_WITH)  
+    )
+    fifo_lcd_unit
+    (
+    .clk(clk_sys),
+    .reset(reset),
+    .src_data(),    // empty for now;
+    .src_valid(),
+    .src_ready(),
+    .sink_data(lcd_stream_in_pixel_data),
+    .sink_valid(lcd_stream_valid_flag),
+    .sink_ready(lcd_stream_ready_flag)
+    );
+    
+    /* lcd display interface (ILI9341); */
+    core_video_lcd_display
+    #(.PARALLEL_DATA_BITS(LCD_DISPLAY_DATA_WIDTH))
+    lcd_display_unit
+    (
+        // general;
+        .clk(clk_sys),
+        .reset(reset),
+        
+        // IO interface
+        .cs(core_ctrl_cs_array[`V0_DISP_LCD]),
+        .write(core_ctrl_wr_array[`V0_DISP_LCD]),
+        .read(core_ctrl_rd_array[`V0_DISP_LCD]),
+        .addr(core_addr_reg_array[`V0_DISP_LCD]),
+        .wr_data(core_data_wr_array[`V0_DISP_LCD]),
+        .rd_data(core_data_rd_array[`V0_DISP_LCD]),
+        
+        /* hw pin specific to the lcd controller */
+        .lcd_drive_wrx(lcd_drive_wrx),     //  to drive the lcd for write op;
+        .lcd_drive_rdx(lcd_drive_rdx),     // to drive the lcd for read op;
+        .lcd_drive_csx(lcd_drive_csx),     // chip seletc;
+        .lcd_drive_dcx(lcd_drive_dcx),     // data or command; LOW for command;          
+        .lcd_dinout(lcd_dinout),   // this is shared between the host and the lcd;
+        
+        /* interface between the fifo */
+        .stream_in_pixel_data(lcd_stream_in_pixel_data),
+        .stream_valid_flag(lcd_stream_valid_flag),       // a lcd start write request from the fifo;
+        .stream_ready_flag(lcd_stream_ready_flag)    // request a read from the fifo for more pixel;
+    );
+    
+    
     
     
     /* ground the the read data signals from the unconstructed video cores 
