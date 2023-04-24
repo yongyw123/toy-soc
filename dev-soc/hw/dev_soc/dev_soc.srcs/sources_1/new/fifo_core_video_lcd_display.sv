8d0f9f5c2d7cf1e628116e02aca91ed9b1ad0020
yyick <yongyickw@gmail.com>
2023-04-24 16:32:05 +1000

    changed fifo

--- dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/fifo_core_video_lcd_display.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/fifo_core_video_lcd_display.sv
@@ -23,7 +23,6 @@
 Construction: 
 this is a FIFO buffer between the pixel generation (source) and the sink (lcd)
 via the core_video_lcd_display_module();
-it uses Xilinx synchronous FIFO
 
 Sink Device:
 1. LCD ILI9341
@@ -33,7 +32,8 @@ Sink Device:
 
 module fifo_core_video_lcd_display
     #(parameter
-        DATA_WIDTH = 8     // this corresponds to LCD parallel 8-bit;
+        DATA_WIDTH = 8,     // this corresponds to LCD parallel 8-bit;
+        ADDR_WIDTH = 5  // 2^5 = 32 fifo depth;
     )
     (
         // general;
@@ -55,62 +55,25 @@ module fifo_core_video_lcd_display
     
     logic almost_empty; // not used;
     logic almost_full;
-    logic full;         // not used;
-    logic empty;
-    logic [10:0] rd_cnt;    // not used;
-    logic [10:0] wr_cnt;    // not used;
-    logic wr_err;           // not used;
-    logic rd_err;           // not used;
+    logic flag_full;         // not used;
+    logic flag_empty;
     
-    assign sink_valid = !empty;         // as soon as the fifo has some data;
-    assign src_ready = !almost_full;    // not when the fifo is already full;
+    assign sink_valid = !flag_empty;  // as soon as the fifo has some data;
+    assign src_ready = !flag_full;    
     
-    
-    //  <-----Cut code below this line---->
-
-   // FIFO_SYNC_MACRO: Synchronous First-In, First-Out (FIFO) RAM Buffer
-   //                  Artix-7
-   // Xilinx HDL Language Template, version 2021.2
-
-   /////////////////////////////////////////////////////////////////
-   // DATA_WIDTH | FIFO_SIZE | FIFO Depth | RDCOUNT/WRCOUNT Width //
-   // ===========|===========|============|=======================//
-   //   37-72    |  "36Kb"   |     512    |         9-bit         //
-   //   19-36    |  "36Kb"   |    1024    |        10-bit         //
-   //   19-36    |  "18Kb"   |     512    |         9-bit         //
-   //   10-18    |  "36Kb"   |    2048    |        11-bit         //
-   //   10-18    |  "18Kb"   |    1024    |        10-bit         //
-   //    5-9     |  "36Kb"   |    4096    |        12-bit         //
-   //    5-9     |  "18Kb"   |    2048    |        11-bit         //
-   //    1-4     |  "36Kb"   |    8192    |        13-bit         //
-   //    1-4     |  "18Kb"   |    4096    |        12-bit         //
-   /////////////////////////////////////////////////////////////////
-
-   FIFO_SYNC_MACRO  #(
-      .DEVICE("7SERIES"), // Target Device: "7SERIES" 
-      .ALMOST_EMPTY_OFFSET(9'h080), // Sets the almost empty threshold
-      .ALMOST_FULL_OFFSET(9'h080),  // Sets almost full threshold
-      .DATA_WIDTH(DATA_WIDTH), // Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
-      .DO_REG(0),     // Optional output register (0 or 1)
-      .FIFO_SIZE ("18Kb")  // Target BRAM: "18Kb" or "36Kb" 
-   ) fifo_unit (
-      .ALMOSTEMPTY(almost_empty), // 1-bit output almost empty
-      .ALMOSTFULL(almost_full),   // 1-bit output almost full
-      .DO(sink_data),                   // Output data, width defined by DATA_WIDTH parameter
-      .EMPTY(empty),             // 1-bit output empty
-      .FULL(full),               // 1-bit output full
-      .RDCOUNT(rd_cnt),         // Output read count, width determined by FIFO depth
-      .RDERR(rd_err),             // 1-bit output read error
-      .WRCOUNT(wr_cnt),         // Output write count, width determined by FIFO depth
-      .WRERR(wr_err),             // 1-bit output write error
-      .CLK(clk),                 // 1-bit input clock
-      .DI(src_data),                   // Input data, width defined by DATA_WIDTH parameter
-      .RDEN(sink_ready),               // 1-bit input read enable
-      .RST(reset),                 // 1-bit input reset
-      .WREN(src_valid)                // 1-bit input write enable
-    );
-
-   // End of FIFO_SYNC_MACRO_inst instantiation
-				
-				
+    FIFO
+    #(.DATA_WIDTH(DATA_WIDTH),
+    .ADDR_WIDTH(ADDR_WIDTH))
+    fifo_unit
+    (
+        .clk(clk),
+        .reset(reset),
+        .ctrl_rd(sink_ready), // read request;
+        .ctrl_wr(src_valid), // write request;
+        .flag_empty(flag_empty),
+        .flag_full(flag_full),
+        
+        .rd_data(sink_data),
+        .wr_data(src_data)
+	);			
 endmodule
