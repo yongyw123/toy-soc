783c3804f86a1edaf1d186895170ab855db33257
yyick <yongyickw@gmail.com>
2023-04-27 18:01:06 +1000

    fixed extern object issues

--- dev-soc/sw/user_src/device_driver/device_directive.cpp
+++ dev-soc/sw/user_src/device_driver/device_directive.cpp
@@ -2,6 +2,6 @@
 #include "device_directive.h"
 
 
-// global declaration;
+// global definition;
 // gpio core is mainly for HW reset pin for the external devices;
-extern core_gpio obj_gpio(GET_MMIO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S4_GPIO_PORT));
+core_gpio obj_gpio(GET_MMIO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S4_GPIO_PORT));
--- dev-soc/sw/user_src/device_driver/device_directive.h
+++ dev-soc/sw/user_src/device_driver/device_directive.h
@@ -26,7 +26,7 @@ extern "C" {
 
 // global declaration;
 // gpio core is mainly for HW reset pin for the external devices;
-core_gpio obj_gpio(GET_MMIO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S4_GPIO_PORT));
+extern core_gpio obj_gpio;
 
 
 #ifdef __cpluscplus
