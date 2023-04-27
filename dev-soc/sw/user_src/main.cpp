bba3febc2519f6bd1d7b3c839806d46ba5dcc90f
yyick <yongyickw@gmail.com>
2023-04-28 02:19:20 +1000

    tried displaying some colour to the LCD on the main app BUT FAILED, debugging

--- dev-soc/sw/user_src/main.cpp
+++ dev-soc/sw/user_src/main.cpp
@@ -8,13 +8,27 @@ core_spi obj_spi(GET_MMIO_CORE_ADDR(BUS_MICROBLAZE_IO_BASE_ADDR_G, S5_SPI));
 
 int main(){
     
-    //delay_busy_ms(2000);
+    delay_busy_ms(2000);
+    
+    // chip select the lcd;
+    lcd_ili9341_enable();
     
     // to read LCD ILI9341 ids;
     lcd_ili9341_read_id();
         
+    // initialize the lcd;
+    lcd_ili9341_init();
+
+    // orientation;
+    lcd_ili9341_set_orientation(0,0,0,0);
+
+    // try painting the lcd with some colours;
+    lcd_ili9341_fill_colour(RGB565_COLOUR_ORANGE);
+
+    debug_str("done??\r\n");
     while(1){
-        ;    
+        ;
+        
     }
 }
 
