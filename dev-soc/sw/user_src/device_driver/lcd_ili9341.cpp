f47fafe62d50b04c3339e97350419725a83450d7
yyick <yongyickw@gmail.com>
2023-04-28 02:58:09 +1000

    changed colour filling lcd function but still buggy

--- dev-soc/sw/user_src/device_driver/lcd_ili9341.cpp
+++ dev-soc/sw/user_src/device_driver/lcd_ili9341.cpp
@@ -442,14 +442,18 @@ void lcd_ili9341_fill_colour(uint16_t mono_colour){
 	 */
 
 	// variable declarations;
-	uint32_t index;
+	//uint32_t index;
+	uint32_t i;	// loop index;
+	uint32_t j;	// loop index;
 	obj_lcd.write_command(LCD_ILI9341_OP_END);
 
 	// command the LCD to accept write before writing the pixels;
 	obj_lcd.write_command(LCD_ILI9341_REG_MEM_WRITE);
 
 	// start filling up the entire LCD with the given colour;
-	for(index = 0; index < LCD_ILI9341_PIXEL_NUM; index++){
+	//for(index = 0; index < LCD_ILI9341_PIXEL_NUM; index++){
+	for(i = 0; i < LCD_ILI9341_DIMENSION_LOW_240; i++){
+		for(j = 0; j < LCD_ILI9341_DIMENSION_HIGH_320; j++)
 		lcd_ili9341_write_pixel(mono_colour);
 	}
 	// terminate the memory write operation;
