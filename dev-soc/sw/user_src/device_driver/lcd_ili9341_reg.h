a7e291ec43192b72488245ff3b41d8b58654e4ca
yyick <yongyickw@gmail.com>
2023-04-27 23:03:07 +1000

    updated ctrl reg list for lcd ili9341

--- dev-soc/sw/user_src/device_driver/lcd_ili9341_reg.h
+++ dev-soc/sw/user_src/device_driver/lcd_ili9341_reg.h
@@ -1,97 +1,79 @@
-/*-------------------------------------
-* Registers for LCD LCD_ILI9341
-* 
-* Acknowledgement:
-* 1. Adapted from Adafruit;
-* 2. Author : Adafruit Community
-* 3. URL    : https://github.com/adafruit/TFTLCD-Library/blob/master/registers.h
---------------------------------------*/
-#define LCD_ILI9341_START_OSC 0x00
-#define LCD_ILI9341_DRIV_OUT_CTRL 0x01
-#define LCD_ILI9341_DRIV_WAV_CTRL 0x02
-#define LCD_ILI9341_ENTRY_MOD 0x03
-#define LCD_ILI9341_RESIZE_CTRL 0x04
-#define LCD_ILI9341_DISP_CTRL1 0x07
-#define LCD_ILI9341_DISP_CTRL2 0x08
-#define LCD_ILI9341_DISP_CTRL3 0x09
-#define LCD_ILI9341_DISP_CTRL4 0x0A
-#define LCD_ILI9341_RGB_DISP_IF_CTRL1 0x0C
-#define LCD_ILI9341_FRM_MARKER_POS 0x0D
-#define LCD_ILI9341_RGB_DISP_IF_CTRL2 0x0F
-#define LCD_ILI9341_POW_CTRL1 0x10
-#define LCD_ILI9341_POW_CTRL2 0x11
-#define LCD_ILI9341_POW_CTRL3 0x12
-#define LCD_ILI9341_POW_CTRL4 0x13
-#define LCD_ILI9341_GRAM_HOR_AD 0x20
-#define LCD_ILI9341_GRAM_VER_AD 0x21
-#define LCD_ILI9341_RW_GRAM 0x22
-#define LCD_ILI9341_POW_CTRL7 0x29
-#define LCD_ILI9341_FRM_RATE_COL_CTRL 0x2B
-#define LCD_ILI9341_GAMMA_CTRL1 0x30
-#define LCD_ILI9341_GAMMA_CTRL2 0x31
-#define LCD_ILI9341_GAMMA_CTRL3 0x32
-#define LCD_ILI9341_GAMMA_CTRL4 0x35
-#define LCD_ILI9341_GAMMA_CTRL5 0x36
-#define LCD_ILI9341_GAMMA_CTRL6 0x37
-#define LCD_ILI9341_GAMMA_CTRL7 0x38
-#define LCD_ILI9341_GAMMA_CTRL8 0x39
-#define LCD_ILI9341_GAMMA_CTRL9 0x3C
-#define LCD_ILI9341_GAMMA_CTRL10 0x3D
-#define LCD_ILI9341_HOR_START_AD 0x50
-#define LCD_ILI9341_HOR_END_AD 0x51
-#define LCD_ILI9341_VER_START_AD 0x52
-#define LCD_ILI9341_VER_END_AD 0x53
-#define LCD_ILI9341_GATE_SCAN_CTRL1 0x60
-#define LCD_ILI9341_GATE_SCAN_CTRL2 0x61
-#define LCD_ILI9341_GATE_SCAN_CTRL3 0x6A
-#define LCD_ILI9341_PART_IMG1_DISP_POS 0x80
-#define LCD_ILI9341_PART_IMG1_START_AD 0x81
-#define LCD_ILI9341_PART_IMG1_END_AD 0x82
-#define LCD_ILI9341_PART_IMG2_DISP_POS 0x83
-#define LCD_ILI9341_PART_IMG2_START_AD 0x84
-#define LCD_ILI9341_PART_IMG2_END_AD 0x85
-#define LCD_ILI9341_PANEL_IF_CTRL1 0x90
-#define LCD_ILI9341_PANEL_IF_CTRL2 0x92
-#define LCD_ILI9341_PANEL_IF_CTRL3 0x93
-#define LCD_ILI9341_PANEL_IF_CTRL4 0x95
-#define LCD_ILI9341_PANEL_IF_CTRL5 0x97
-#define LCD_ILI9341_PANEL_IF_CTRL6 0x98
+/*
+ * LCD ILI9341 control registr;
+ *  Reference:
+ *  1. Datasheet: https://cdn-shop.adafruit.com/datasheets/ILI9341.pdf
+ */
 
-#define LCD_ILI9341_SOFTRESET 0x01
-#define LCD_ILI9341_SLEEPIN 0x10
-#define LCD_ILI9341_SLEEPOUT 0x11
-#define LCD_ILI9341_NORMALDISP 0x13
-#define LCD_ILI9341_INVERTOFF 0x20
-#define LCD_ILI9341_INVERTON 0x21
-#define LCD_ILI9341_GAMMASET 0x26
-#define LCD_ILI9341_DISPLAYOFF 0x28
-#define LCD_ILI9341_DISPLAYON 0x29
-#define LCD_ILI9341_COLADDRSET 0x2A
-#define LCD_ILI9341_PAGEADDRSET 0x2B
-#define LCD_ILI9341_MEMORYWRITE 0x2C
-#define LCD_ILI9341_PIXELFORMAT 0x3A
-#define LCD_ILI9341_FRAMECONTROL 0xB1
-#define LCD_ILI9341_DISPLAYFUNC 0xB6
-#define LCD_ILI9341_ENTRYMODE 0xB7
-#define LCD_ILI9341_POWERCONTROL1 0xC0
-#define LCD_ILI9341_POWERCONTROL2 0xC1
-#define LCD_ILI9341_VCOMCONTROL1 0xC5
-#define LCD_ILI9341_VCOMCONTROL2 0xC7
-#define LCD_ILI9341_MEMCONTROL 0x36
-#define LCD_ILI9341_MADCTL 0x36
+#ifndef LCD_ILI9341_REG_H
+#define LCD_ILI9341_REG_H
 
-#define LCD_ILI9341_MADCTL_MY 0x80
-#define LCD_ILI9341_MADCTL_MX 0x40
-#define LCD_ILI9341_MADCTL_MV 0x20
-#define LCD_ILI9341_MADCTL_ML 0x10
-#define LCD_ILI9341_MADCTL_RGB 0x00
-#define LCD_ILI9341_MADCTL_BGR 0x08
-#define LCD_ILI9341_MADCTL_MH 0x04
+/* no operation / dummy */
+#define LCD_ILI9341_REG_NOP	0x00				// NOP; useful to end "ongoing ILI9341 operations" ;
+#define LCD_ILI9341_OP_END LCD_ILI9341_REG_NOP 	// just a wrapper by above;
 
+/* reset */
+#define LCD_ILI9341_REG_SW_RESET 0x01			// soft reset;
+
+/* LCD pixel format: 16-bit or 18-bit? */
+#define LCD_ILI9341_REG_PIXEL_FORMAT 0x3A    	// COLMOD: pixel format set
+
+/* display */
+#define LCD_ILI9341_REG_DISP_OFF 0x28  			// display off;
+#define LCD_ILI9341_REG_DISP_ON 0x29   			// display on;
+#define LCD_ILI9341_REG_DISP_INV_OFF 0x20   	// display inversion off;
+#define LCD_ILI9341_REG_DISP_INV_ON 0x21    	// display inversion on;
+
+/* mode */
+#define LCD_ILI9341_REG_SLEEP_IN 0x10  			// enter sleep
+#define LCD_ILI9341_REG_SLEEP_OUT 0x11 			// exit sleep
+#define LCD_ILI9341_REG_NORMAL_DISP_ON 0x13  	// normal display mode on;
+
+/* memory addressing */
+#define LCD_ILI9341_REG_ADDR_COL_SET 0x2A 	// column address set;
+#define LCD_ILI9341_REG_ADDR_PAGE_SET 0x2B 	// page address set;
+#define LCD_ILI9341_REG_MEM_WRITE 0x2C 		// memory write;
+#define LCD_ILI9341_REG_MAC 0x36			// memory access control;
+
+/* frame rate control */
+#define LCD_ILI9341_REG_FRMCTR_1 0xB1 // frame rate control (normal mode/full colors);
+#define LCD_ILI9341_REG_FRMCTR_2 0xB2 // frame rate control (idle mode/8 colors);
+#define LCD_ILI9341_REG_FRMCTR_3 0xB3 // frame rate control (partial mode/full colors);
+
+/* power control */
+#define LCD_ILI9341_REG_POWCTR_SEQ 0xED // power on control sequence;
+#define LCD_ILI9341_REG_POWCTR_A 0xCB 	// power control A
+#define LCD_ILI9341_REG_POWCTR_B 0xCF 	// power control B
+#define LCD_ILI9341_REG_POWCTR_1 0xC0 	// power control 1
+#define LCD_ILI9341_REG_POWCTR_2 0xC1 	// power control 2
+#define LCD_ILI9341_REG_POWCTR_3 0xC2 	// power control 3
+#define LCD_ILI9341_REG_POWCTR_4 0xC3 	// power control 4
+#define LCD_ILI9341_REG_POWCTR_5 0xC4 	// power control 5
+#define LCD_ILI9341_REG_PRC 0xF7		// pump ratio control;
+#define LCD_ILI9341_REG_VCOM_1 0xC5 	// VCOM 1
+#define LCD_ILI9341_REG_VCOM_2 0xC7 	// VCOM 2
+
+/* driver timing control */
+#define LCD_ILI9341_REG_DTC_A 0xE8	// driver timing control A;
+#define LCD_ILI9341_REG_DTC_B 0xEA	// driver timing control B;
+
+/* gamma (curve) control */
+#define LCD_ILI9341_REG_GAMMA_SET 0x26		// to select the desired Gamma curve for the current display
+#define LCD_ILI9341_REG_GAMMA_THREE	0xF2	// enable 3 gamma;
+#define LCD_ILI9341_REG_GAMMA_POSITIVE 0xE0 // positive gamma correction;
+#define LCD_ILI9341_REG_GAMMA_NEGATIVE 0xE1 // negative gamma correction;
+
+/* interface control */
+#define LCD_ILI9341_REG_DFC 0xB6				// display function control;
+#define LCD_ILI9341_REG_RGB_INTERFACE_CTR 0xB0	// RGB interface control
+#define LCD_ILI9341_REG_INTERFACE_CTR 0xF6		// interface control;
+
+/* read id */
 // read manufacturer id;
 #define LCD_ILI9341_RDID1   0xDA    // read id 1;
 #define LCD_ILI9341_RDID2   0xDB    // read id 2;
 #define LCD_ILI9341_RDID3   0xDC    // read id 3;
 
 // read ic device code;
-#define LCD_ILI9341_RDID4   0xD3    
+#define LCD_ILI9341_RDID4   0xD3  
+
+#endif // LCD_ILI9341_REG_H
