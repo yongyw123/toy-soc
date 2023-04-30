46905b5003f0f061cd41e8c9e8c38e40a9224219
yyick <yongyickw@gmail.com>
2023-04-30 22:11:50 +1000

    drafted class header for video core source multiplexing

---
+++ dev-soc/sw/user_src/video_driver/video_core_src_mux.h
@@ -0,0 +1,24 @@
+#ifndef _VIDEO_CORE_SRC_MUX_H
+#define _VIDEO_CORE_SRC_MUX_H
+
+/* ---------------------------------------------
+Purpose : SW drivers for HW module as follows
+Module  : core_video_src_mux.sv
+---------------------------------------------*/
+#include "io_map.h"
+#include "io_reg_util.h"
+
+// c and cpp linkage;
+// reference: https://igl.ethz.ch/teaching/tau/resources/cprog.htm
+#ifdef __cpluscplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cpluscplus
+} // extern "C";
+#endif
+
+
+#endif //_VIDEO_CORE_SRC_MUX_H
