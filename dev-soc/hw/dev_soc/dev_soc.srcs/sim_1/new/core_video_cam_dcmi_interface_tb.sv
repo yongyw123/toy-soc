ccc7bdd79a635bfaec04f71651958858322b8cd4
yyick <yongyickw@gmail.com>
2023-05-04 02:40:55 +1000

    drafted tb for video core dcmi interface

--- dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_video_cam_dcmi_interface_tb.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_video_cam_dcmi_interface_tb.sv
@@ -19,8 +19,50 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+`ifndef CORE_VIDEO_CAM_DCMI_INTERFACE_TB_SV
+`define CORE_VIDEO_CAM_DCMI_INTERFACE_TB_SV
 
-module core_video_cam_dcmi_interface_tb(
+`include "IO_map.svh"
 
+
+program core_video_cam_dcmi_interface_tb
+    (
+        input logic clk_sys,
+        
+        output logic cs,
+        output logic write,
+        output logic read,
+        output logic [`VIDEO_REG_ADDR_BIT_SIZE_G-1:0] addr,        
+        output logic [`REG_DATA_WIDTH_G-1:0]  wr_data,    
+        input logic [`REG_DATA_WIDTH_G-1:0]  rd_data,
+        
+        output logic sink_ready
+            
     );
-endmodule
+    // constanst
+    localparam REG_CTRL_OFFSET                  = 3'b000;
+    localparam REG_DECODER_STATUS_OFFSET        = 3'b001;
+    localparam REG_FRAME_OFFSET                 = 3'b010;
+    localparam REG_FIFO_STATUS_OFFSET           = 3'b011;
+    localparam REG_FIFO_CNT_OFFSET              = 3'b100;    
+    localparam REG_FIFO_SYS_INIT_STATUS_OFFSET  = 3'b101;
+    
+    initial begin
+    @(posedge clk_sys);
+    cs <= 1'b1;
+    write <= 1'b1;
+    read <= 1'b0;
+    addr <= REG_CTRL_OFFSET;
+    wr_data <= 3'b110;
+    
+    
+    
+    
+    #(1000);
+    $display("test ends");
+    $stop;
+    
+    end
+endprogram
+
+`endif //CORE_VIDEO_CAM_DCMI_INTERFACE_TB_SV
--- dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_video_cam_dcmi_interface_top_tb.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_video_cam_dcmi_interface_top_tb.sv
@@ -19,8 +19,120 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+`ifndef CORE_VIDEO_CAM_DCMI_INTERFACE_TOP_TB_SV
+`define CORE_VIDEO_CAM_DCMI_INTERFACE_TOP_TB_SV
 
-module core_video_cam_dcmi_interface_top_tb(
+`include "IO_map.svh"
 
-    );
+module core_video_cam_dcmi_interface_top_tb();
+    // general;
+    localparam T = 10;  // system clock period: 10ns;
+    logic clk_sys;          // common system clock;
+    logic reset_sys;        // async system clock;
+    
+    // uut signals;
+    localparam DATA_BITS = 8;
+    localparam HREF_COUNTER_WIDTH  = 8;    // to count href;
+    localparam HREF_TOTAL          = 240;  // expected to have 240 href for a line;
+    localparam FRAME_COUNTER_WIDTH = 32;    // count the number of frames;
+    
+    // for HW DCMI emulator;
+    localparam PCLK_MOD            = 4;    // 100/4 = 25;
+    localparam VSYNC_LOW           = 10;   //vlow;
+    localparam HREF_LOW            = 5;    // hlow; 
+    localparam BUFFER_START_PERIOD = 7;    // between vsync assertion and href assertion;
+    localparam BUFFER_END_PERIOD   = 5;	// between the frame end and the frame start;
+    localparam PIXEL_BYTE_TOTAL    = 640;   // 320 pixels per href with bp = 16-bit; 
+
+    logic cs;    
+    logic write;              
+    logic read;               
+    logic [`VIDEO_REG_ADDR_BIT_SIZE_G-1:0] addr;  //  19-bit;         
+    logic [`REG_DATA_WIDTH_G-1:0]  wr_data;    
+    logic [`REG_DATA_WIDTH_G-1:0]  rd_data;
+    
+    // specific external  signals;
+    logic CAM_PCLK;
+    logic CAM_HREF;
+    logic CAM_VSYNC;
+    logic [DATA_BITS-1:0] CAM_DIN;
+    
+    // for downstream signals;
+    logic [DATA_BITS-1:0] stream_out_data;
+    logic sink_ready;     // signal from the sink to this interface;
+    logic sink_valid;     // signal from this interface to the sink;
+    
+    // for debugging;
+    logic debug_RST_FIFO;
+    logic debug_FIFO_rst_ready;
+    
+    /* instantiation */
+    core_video_cam_dcmi_interface
+    #(
+        .DATA_BITS(DATA_BITS),           
+        .HREF_COUNTER_WIDTH(HREF_COUNTER_WIDTH),  
+        .HREF_TOTAL(HREF_TOTAL),          
+        .FRAME_COUNTER_WIDTH(FRAME_COUNTER_WIDTH), 
+        
+        // for HW DCMI emulator;
+        .PCLK_MOD(PCLK_MOD),            
+        .VSYNC_LOW(VSYNC_LOW),           
+        .HREF_LOW(HREF_LOW),            
+        .BUFFER_START_PERIOD(BUFFER_START_PERIOD), 
+        .BUFFER_END_PERIOD(BUFFER_END_PERIOD),
+        .PIXEL_BYTE_TOTAL(PIXEL_BYTE_TOTAL)
+     )
+     uut
+     (
+        .clk_sys(clk_sys),    // 100 MHz;
+        .reset_sys(reset_sys),  // async;
+        
+        //> given interface with video controller (which interfaces with the bus);
+        // note that not all interfacce will be used;
+        .cs(cs),    
+        .write(write),              
+        .read(read),               
+        .addr(addr),  //  19-bit;         
+        .wr_data(wr_data),    
+        .rd_data(rd_data),
+        
+        // specific external  signals;
+        .CAM_PCLK(),
+        .CAM_HREF(),
+        .CAM_VSYNC(),
+        .CAM_DIN(),
+        
+        // for downstream signals;
+        .stream_out_data(stream_out_data),
+        .sink_ready(sink_ready),     // signal from the sink to this interface;
+        .sink_valid(sink_valid),     // signal from this interface to the sink;
+        
+        // for debugging;
+        .debug_RST_FIFO(debug_RST_FIFO),
+        .debug_FIFO_rst_ready(debug_FIFO_rst_ready)
+     );
+     
+     // test stimulus;
+     core_video_cam_dcmi_interface_tb tb(.*);
+     
+    /* simulate system clk */
+     always
+        begin 
+           clk_sys = 1'b1;  
+           #(T/2); 
+           clk_sys = 1'b0;  
+           #(T/2);
+        end
+    
+    /* reset pulse */
+     initial
+        begin
+            reset_sys = 1'b1;
+            #(T/2);
+            reset_sys = 1'b0;
+            #(T/2);
+        end
+    
 endmodule
+
+`endif //CORE_VIDEO_CAM_DCMI_INTERFACE_TOP_TB_SV
