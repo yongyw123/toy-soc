3807775173d1d2eb945543527132b19999cc48cf
yyick <yongyickw@gmail.com>
2023-05-03 23:52:33 +1000

    created tb for video core dcmi decoder

---
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_video_cam_dcmi_interface_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.05.2023 17:45:28
+// Design Name: 
+// Module Name: core_video_cam_dcmi_interface_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module core_video_cam_dcmi_interface_tb(
+
+    );
+endmodule
---
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sim_1/new/core_video_cam_dcmi_interface_top_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.05.2023 17:44:55
+// Design Name: 
+// Module Name: core_video_cam_dcmi_interface_top_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module core_video_cam_dcmi_interface_top_tb(
+
+    );
+endmodule
