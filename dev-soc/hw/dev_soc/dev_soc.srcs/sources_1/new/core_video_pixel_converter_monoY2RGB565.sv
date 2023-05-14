2aff3b7dadbf8fa3a08c880a4bf4a7357464d92a
yyick <yongyickw@gmail.com>
2023-05-14 19:56:07 +1000

    updated video core wrapper for the pixel converter

--- dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/core_video_pixel_converter_monoY2RGB565.sv
+++ dev-soc/hw/dev_soc/dev_soc.srcs/sources_1/new/core_video_pixel_converter_monoY2RGB565.sv
@@ -81,10 +81,103 @@ module core_video_pixel_converter_monoY2RGB565
         output logic [BITS_PER_PIXEL_8B - 1:0] sink_data
     );
     
+    // constants;
+    localparam ENABLE_CONVERTER = 1'b1;
+    localparam DISABLE_CONVERTER = 1'b0;
     
-    /* declaration */
+    /*----------- declaration */   
+    // interface between the pixel converter and the selector;
+    logic [BITS_PER_PIXEL_16B-1:0] converted_rgb565;
+    
+    // interface betwene the upstream mux and the pixel selector;
+    logic src_valid_pixel_selector;
+    logic src_ready_pixel_selector;
+    
+    
+    // interface between the pixel selector and downstream mux; 
+    logic [BITS_PER_PIXEL_8B-1:0] converter_rgb565_downstream;
+    logic sink_ready_pixel_converter;
+    logic sink_valid_pixel_converter;
     
     // registers;
+    logic ctrl_reg, ctrl_next;
     
+    // ff;
+    always_ff @(posedge clk, posedge reset) begin
+        if(reset) begin
+            ctrl_reg <= 1'b0;   // colour converter disabled by default;
+        end
+        else begin
+            ctrl_reg <= wr_data[0];
+        end    
+    end
+    
+    /* ----------------------------
+    * upstream multiplexer
+    -----------------------------*/
+    always_comb begin
+        // bypass the converter; 
+        if(ctrl_reg == DISABLE_CONVERTER) begin
+            src_valid_pixel_selector = 1'b0;    
+            src_ready = sink_ready;
+        end
+        // go through the converter;
+        else begin
+            src_valid_pixel_selector = src_valid;
+            src_ready = src_ready_pixel_selector;
+        end
+    end
+    
+    /* ----------------------------
+    * downstream multiplexer
+    -----------------------------*/
+    always_comb begin
+        // bypass the converter;
+        if(ctrl_reg == DISABLE_CONVERTER) begin
+            sink_data = src_data;
+            sink_valid = src_valid;   
+            sink_ready_pixel_converter = 1'b0;     
+        end
+        // go through the converter;
+        else begin
+            sink_data = converter_rgb565_downstream;
+            sink_valid = sink_valid_pixel_converter;
+            sink_ready_pixel_converter = sink_ready;        
+        end
+    end
+    
+    /* ------------ instantiation */
+    
+    // converter
+    pixel_mono_YUV422_to_RGB565
+    converter_unit(
+        .pixel_in(src_data),
+        .rgb565_out(converted_rgb565)
+    );
+    
+    
+    // pixel selector;
+    pixel_Y2RGB565_pass
+    (
+        // general;
+        .clk_sys(clk),    // 100Mhz;
+        .reset(reset),      // async;
+        
+        // interface with the upper stream;
+        .src_valid(src_valid_pixel_selector),
+        .src_ready(src_ready_pixel_selector),
+        
+        // interface with the upper conversion block;
+        .converted_rgb565_in(converted_rgb565),
+        
+        // interface with the down stream;
+        .sink_ready(sink_ready_pixel_converter),
+        .sink_valid(sink_valid_pixel_converter),
+        .rgb565_out(converter_rgb565_downstream)        
+    );
+       
+
+    // read out;    
+    assign rd_data = {31'b0, ctrl_reg};
     
 endmodule
